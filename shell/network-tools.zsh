3e3d30c3a9fef66c609ed44a23a2bd0d8863fa00
Thiraphat <thirphathrsricitr9@gmail.com>
2026-05-18 21:21:37 +0700

    clear nt alias in shell integration

--- shell/network-tools.zsh
+++ shell/network-tools.zsh
@@ -1,5 +1,7 @@
 # Network Tools shell integration.
 
+unalias nt 2>/dev/null
+
 network_tools_completion_dir="$HOME/.config/network-tools/completions"
 
 if [[ -d "$network_tools_completion_dir" ]]; then
